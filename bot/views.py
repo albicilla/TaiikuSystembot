15d3be6d15d4951b9c19f82a2fd63ba61f382895
albic <albic@sfc.wide.ad.jp>
2017-07-19 19:26:29 +0900

    heroku用u33

--- bot/views.py
+++ bot/views.py
@@ -10,7 +10,8 @@ from django.http import HttpResponse
 import sys
 path="/Users/albicilla/programming/osoBOT/osomatsu_bot/bot/"
 sys.path.append(path)
-
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 from load_serif import osomatsu_serif  # 先ほどのおそ松のセリフ一覧をimport
 import re #正規表現
@@ -40,22 +41,140 @@ def index(request):
     return HttpResponse("This is bot api.")
 
 
-
 def reply_text(reply_token, text):
 
-    #config.txtを読み込む
-    config = ConfigParser.SafeConfigParser()
-    conf = os.path.join(os.path.dirname(__file__),'config.txt')
-    config.read([os.path.expanduser(conf)])
+
 
     # show 予約情報の表示
 
 
-    # 説明　正規表現
-    explain = re.compile("explain*")
+    # 予約　正規表現
+    regi = re.compile("regi*.")
 
     reply = ""
     if text == "show":
+        #config.txtを読み込む
+        config = ConfigParser.SafeConfigParser()
+        conf = os.path.join(os.path.dirname(__file__),'config.txt')
+        config.read([os.path.expanduser(conf)])
+        #scraping
+        browser = mechanize.Browser()
+        #robot.txtを無効化しておくとちゃんと開ける
+        browser.set_handle_robots(False)
+        #URLを指定して読み込み　体育システムのURL
+
+
+        #取得したhtmlを出力
+            #print html.read()
+        TOP_URL = 'https://wellness.sfc.keio.ac.jp/v3/'
+        browser.open('https://wellness.sfc.keio.ac.jp/v3/')
+
+        #0番目のフォームを選択
+        browser.select_form(nr=0)
+        #洗濯したフォームに値を設定
+        browser.form['login'] = config.get('config','name')
+        browser.form['password'] = config.get('config','password')
+        #submitする
+        res = browser.submit()
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+        #lineに通知
+        line_message =''
+        tables = soup.find_all('tr')
+        idx = 0
+        for sk in tables:
+            p = sk.findAll('td')
+            for pk in p:
+                if re.match(u'[シ]',pk.text) or re.match(u'[予約]',pk.text) :
+                    print ("　lkjlk")
+                elif re.match('\(',pk.text):
+                    line_message+="\n"
+                elif re.match('^[0-9]{1,}$',pk.text):
+                    line_message += u" 空き数"
+                    line_message += pk.text
+                    line_message += " ID="
+                    line_message += str([idx])
+                    idx += 1
+                else:
+                    line_message+="\n"
+                    line_message += pk.text
+                    print ("%s" % pk.text)
+
+        reply = line_message;
+    elif re.match(regi,text):
+        print (u"予約処理")
+        #config.txtを読み込む
+        config = ConfigParser.SafeConfigParser()
+        conf = os.path.join(os.path.dirname(__file__),'config.txt')
+        config.read([os.path.expanduser(conf)])
+        #scraping
+        browser = mechanize.Browser()
+        #robot.txtを無効化しておくとちゃんと開ける
+        browser.set_handle_robots(False)
+        #URLを指定して読み込み　体育システムのURL
+
+
+        #取得したhtmlを出力
+            #print html.read()
+        TOP_URL = 'https://wellness.sfc.keio.ac.jp/v3/'
+        browser.open('https://wellness.sfc.keio.ac.jp/v3/')
+
+        #0番目のフォームを選択
+        browser.select_form(nr=0)
+        #洗濯したフォームに値を設定
+        browser.form['login'] = config.get('config','name')
+        browser.form['password'] = config.get('config','password')
+        #submitする
+        res = browser.submit()
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+        #lineに通知
+        line_message =''
+        tables = soup.find_all('tr')
+
+        #パースして上からいくつ目のものを知りたいかをパラメタに与える
+        print (text)
+        #正規表現
+        pattern=r'([+-]?[0-9]+\.?[0-9]*)'
+        ynum = re.findall(pattern,text)
+        print (ynum[0])
+        yidx = 0
+        yoyaku_url=""
+        for sk in tables:
+            a = sk.findAll('a')
+            for ak in a:
+                if(ak.text == u"予約"):
+                    if yidx == int(ynum[0]):
+                        print("%s's url is %s" % (ak.text, ak.attrs['href']))
+                        yoyaku_url = ak.attrs['href']
+                    yidx += 1
+        res = browser.open(yoyaku_url.decode("utf-8"))
+        browser.select_form(nr=1)
+        res = browser.submit()
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+        #print (soup)
+        res_error = soup.findAll("p", attrs={"class": "error"})[0].text
+        res_error = res_error.strip()
+
+        if res_error == u"同じ週に予約し出席(欠席)できるのは2コマまでです．":
+            print(res_error)
+            return False
+        elif res_error == u"すでに予約済みです．":
+            print(res_error)
+            return False
+        else:
+            browser.select_form(nr=1)
+            browser.submit()# 予約確定
+            print("予約しました")
+
+        reply = "予約しました"
+    elif text == "attend":
+        print (u"確認処理")
+        #config.txtを読み込む
+        config = ConfigParser.SafeConfigParser()
+        conf = os.path.join(os.path.dirname(__file__),'config.txt')
+        config.read([os.path.expanduser(conf)])
         #scraping
         browser = mechanize.Browser()
         #robot.txtを無効化しておくとちゃんと開ける
@@ -78,31 +197,134 @@ def reply_text(reply_token, text):
         res_html = res.read()
         soup = BeautifulSoup(res_html, "html.parser")
 
-        #lineに通知
-        line_message =''
-        tables = soup.find_all('tr',class_='alt')
-        for sk in tables:
-        	p = sk.findAll('td')
-        	for pk in p:
-        		if re.match(u'[シ]',pk.text) or re.match(u'[予約]',pk.text) :
-        			print ("　lkjlk")
-        		elif re.match('\(',pk.text):
-        			line_message+="\n"
-        		elif re.match('^[0-9]{1,}$',pk.text):
-        			line_message += u" 空き数"
-        			line_message += pk.text
-        		else:
-        			line_message+="\n"
-        			line_message += pk.text
-    				print ("%s" % pk.text)
+        res_attend = soup.findAll("div", attrs={"class": "attend"})[0].text
+        res_attend = res_attend.strip()
 
-        reply = line_message;
-    elif text == "regi":
-        for i in range(int(split_text[0])):
-            reply  += str([random.randint(1,int(split_text[1]))])
+        print (res_attend)
+        reply = res_attend
+    elif text == "check":
+        #config.txtを読み込む
+        config = ConfigParser.SafeConfigParser()
+        conf = os.path.join(os.path.dirname(__file__),'config.txt')
+        config.read([os.path.expanduser(conf)])
+        #scraping
+        browser = mechanize.Browser()
+        #robot.txtを無効化しておくとちゃんと開ける
+        browser.set_handle_robots(False)
+        #URLを指定して読み込み　体育システムのURL
+
+
+        #取得したhtmlを出力
+            #print html.read()
+        TOP_URL = 'https://wellness.sfc.keio.ac.jp/v3/'
+        browser.open('https://wellness.sfc.keio.ac.jp/v3/')
+
+        #0番目のフォームを選択
+        browser.select_form(nr=0)
+        #洗濯したフォームに値を設定
+        browser.form['login'] = config.get('config','name')
+        browser.form['password'] = config.get('config','password')
+        #submitする
+        res = browser.submit()
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+
+        tables = soup.find_all('ul')
+        yidx = 0
+        kakunin_url=""
+        line_message=""
+        for sk in tables:
+            a = sk.findAll('a')
+            for ak in a:
+                if yidx == 1:
+                    print("%s's url is %s" % (ak.text, ak.attrs['href']))
+                    kakunin_url = ak.attrs['href']
+                yidx += 1
+        print (kakunin_url)
+        kakunin_url = "https://wellness.sfc.keio.ac.jp"+kakunin_url
+        res = browser.open(kakunin_url.decode("utf-8"))
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+        #print (soup)
+        tables = soup.find_all('tr')
+        idx = 0
+        for sk in tables:
+            p = sk.findAll('td')
+            for pk in p:
+                if re.match(u'[シ]',pk.text) or re.match(u'[予約]',pk.text) :
+                    print ("　lkjlk")
+                elif re.match('\(',pk.text):
+                    line_message+="\n"
+                else:
+                    line_message+="\n"
+                    line_message += pk.text
+                    print ("%s" % pk.text)
+
+        reply = "以下の授業が予約されています\n"
+        reply += line_message
+    elif text=="remove":
+        #config.txtを読み込む
+        config = ConfigParser.SafeConfigParser()
+        conf = os.path.join(os.path.dirname(__file__),'config.txt')
+        config.read([os.path.expanduser(conf)])
+        #scraping
+        browser = mechanize.Browser()
+        #robot.txtを無効化しておくとちゃんと開ける
+        browser.set_handle_robots(False)
+        #URLを指定して読み込み　体育システムのURL
+
+
+        #取得したhtmlを出力
+            #print html.read()
+        TOP_URL = 'https://wellness.sfc.keio.ac.jp/v3/'
+        browser.open('https://wellness.sfc.keio.ac.jp/v3/')
+
+        #0番目のフォームを選択
+        browser.select_form(nr=0)
+        #洗濯したフォームに値を設定
+        browser.form['login'] = config.get('config','name')
+        browser.form['password'] = config.get('config','password')
+        #submitする
+        res = browser.submit()
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+
+        tables = soup.find_all('ul')
+        yidx = 0
+        kakunin_url=""
+        line_message=""
+        for sk in tables:
+            a = sk.findAll('a')
+            for ak in a:
+                if yidx == 1:
+                    print("%s's url is %s" % (ak.text, ak.attrs['href']))
+                    kakunin_url = ak.attrs['href']
+                yidx += 1
+        print (kakunin_url)
+        kakunin_url = "https://wellness.sfc.keio.ac.jp"+kakunin_url
+        res = browser.open(kakunin_url.decode("utf-8"))
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+
+        tables = soup.find_all('p')
+        for sk in tables:
+            if sk.text == u"現在予約している授業はありません．":
+                reply = "現在予約している授業はありません．"
+
+        #チェックボックスにチェックを入れる
+        browser.select_form(nr=1)
+        browser.find_control(type="checkbox").items[0].selected=True
+        res=browser.submit()
+        res_html = res.read()
+        soup = BeautifulSoup(res_html, "html.parser")
+        res_error = soup.findAll("p", attrs={"class": "error"})[0].text
+
+        reply = res_error
+
+
+
     else:
         reply = random.choice(osomatsu_serif)
-
 
     payload = {
           "replyToken":reply_token,
@@ -120,6 +342,7 @@ def reply_text(reply_token, text):
 def callback(request):
     reply = ""
     request_json = json.loads(request.body.decode('utf-8')) # requestの情報をdict形式で取得
+
     for e in request_json['events']:
         reply_token = e['replyToken']  # 返信先トークンの取得
         message_type = e['message']['type']   # typeの取得
